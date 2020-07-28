3d2b33a8c8d316bcfb9446292de82b30dda7e56d
nspevacek <nspevacek@wisc.edu>
2020-07-27 19:12:54 -0500

    4d

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,11 +4,13 @@ require('express-async-errors')
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -5,7 +5,7 @@ const User = require('../models/user')
 usersRouter.get('/', async (request, response) => {
     const users = await User
         .find({}).populate('blogs', { title: 1, author: 1, likes: 1 })
-        
+
     response.json(users)
 })
 
@@ -18,7 +18,7 @@ usersRouter.post('/', async (request, response) => {
     const user = new User({
         username: body.username,
         name: body.name,
-        passwordHash
+        passwordHash,
     })
 
     const savedUser = await user.save()
